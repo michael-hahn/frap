c4d68efb8c2feb22852e01c6e3b89af008895797
Michael Hahn <mike.han@hotmail.com>
2017-03-07 13:28:36 -0500

    change script a bit

--- myapps/dataset2/wgettest_client.sh
+++ myapps/dataset2/wgettest_client.sh
@@ -2,25 +2,36 @@
 
 #This script is to run on the client side of the wget exploit.
 #We assume that wget is of version prior to 1.18; otherwise, no exploit will take place.
-#we also assume that the script is in the superuser mode
+#we also assume that the script is in the superuser mode, and run in /root directory
 #One must input the IP address of the server to retrieve data
+#One must also provide a path to save the provenance file (or comment out the last line and keep it in /tmp/audit.log)
 
-#usage: ./wggettest_client.sh <IP_address>
-
-cd /root
+#usage: ./wgettest_client.sh <IP_address> <destination_file_path>
 
 sudo camflow --track-file /usr/local/bin/wget propagate
 
 wget -N $1
 
-wget -N $1
+sleep 2s
 
 wget -N $1
 
-wget -N $1
+sleep 2s
 
 wget -N $1
 
+sleep 2s
+
 wget -N $1
 
-sudo cp /tmp/audio.log provenance.txt
+sleep 2s
+
+wget -N $1
+
+sleep 2s
+
+wget -N $1
+
+sleep 4s
+
+sudo cp /tmp/audit.log $2
